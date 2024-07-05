21a5e8ce36773ddf55ff36b46f5dd5bf66f727c7
RuvenPinkhasov <rubi00010@gmail.com>
2024-07-05 17:36:55 -0400

    feat: created the user route and added it to the index route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -8,6 +9,8 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {})
   .catch(console.error);
+
+app.use("/", mainRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
