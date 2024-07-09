d6700fbb319fe3db9d91fe931e8f26461aa072ff
RuvenPinkhasov <rubi00010@gmail.com>
2024-07-09 01:12:12 -0400

    feat: created constants for the server responses and added appropriate messages

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,10 +1,19 @@
 const Item = require("../models/clothingItem");
+const {
+  OK,
+  CREATED,
+  BAD_REQUEST,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+} = require("../utils/errors");
 
 const getItems = (req, res) => {
   Item.find({})
-    .then((items) => res.status(200).send(items))
+    .then((items) => res.status(OK).send(items))
     .catch((err) => {
-      return res.status(500).send({ message: err.message });
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
@@ -13,12 +22,17 @@ const createItem = (req, res) => {
   const owner = req.user._id;
 
   Item.create({ name, weather, imageUrl, owner })
-    .then((item) => res.status(201).send(item))
+    .then((item) => res.status(CREATED).send(item))
     .catch((err) => {
       if (err.name === "ValidationError") {
-        return res.status(400).send({ message: err.message });
+        return res.status(BAD_REQUEST).send({
+          message:
+            "invalid data passed to the methods for creating an item, or invalid ID passed to the params.",
+        }); //invalid data passed to the methods for creating an item or updating an item
       }
-      return res.status(500).send({ message: err.message });
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
@@ -27,12 +41,17 @@ const deleteItem = (req, res) => {
   Item.findById(itemId)
     .orFail()
     .then((item) => item.deleteOne())
-    .then(() => res.status(200).send({ message: "Item deleted successfully" }))
+    .then(() => res.status(OK).send({ message: "Item deleted successfully" }))
     .catch((err) => {
       if (err.name === "DocumentNotFoundError") {
-        return res.status(404).send({ message: err.message });
+        return res.status(NOT_FOUND).send({
+          message:
+            "there is no clothing item with the requested id, or the request was sent to a non-existent address.",
+        });
       }
-      return res.status(500).send({ message: err.message });
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,38 @@
 const User = require("../models/user");
+const {
+  OK,
+  CREATED,
+  BAD_REQUEST,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+} = require("../utils/errors");
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send(users))
+    .then((users) => res.status(OK).send(users))
     .catch((err) => {
-      return res.status(500).send({ message: err.message });
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
 const createUser = (req, res) => {
   const { name, avatar } = req.body;
   User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
+    .then((user) => res.status(CREATED).send(user))
     .catch((err) => {
       if (err.name === "ValidationError") {
-        return res.status(400).send({ message: err.message });
+        return res
+          .status(BAD_REQUEST)
+          .send({
+            message:
+              "invalid data passed to the methods for creating a user, or invalid ID passed to the params.",
+          });
       }
-      return res.status(500).send({ message: err.message });
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
@@ -24,15 +40,20 @@ const getUser = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
     .orFail()
-    .then((user) => res.status(200).send(user))
+    .then((user) => res.status(OK).send(user))
     .catch((err) => {
       switch (err.name) {
         case "CastError":
-          return res.status(400).send({ message: err.message });
+          return res.status(BAD_REQUEST).send({ message: err.message });
         case "DocumentNotFoundError":
-          return res.status(404).send({ message: err.message });
+          return res.status(NOT_FOUND).send({
+            message:
+              "there is no user with the requested id, or the request was sent to a non-existent address.",
+          });
         default:
-          return res.status(500).send({ message: err.message });
+          return res
+            .status(INTERNAL_SERVER_ERROR)
+            .send({ message: "An error has occurred on the server." });
       }
     });
 };
