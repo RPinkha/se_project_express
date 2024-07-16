020db3ea5a4bcabb63fd55aaf92b65247198b2a9
RuvenPinkhasov <rubi00010@gmail.com>
2024-07-16 16:15:47 -0400

    feat: created middleware for authorization and protected applicable routes with it

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
+const { UNAUTHORIZED } = require("../utils/errors");
+
+const handleAuthError = (res) => {
+  res.status(UNAUTHORIZED).send({ message: "Authorization Error" });
+};
+
+const extractBearerToken = (header) => {
+  return header.replace("Bearer ", "");
+};
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return handleAuthError(res);
+  }
+  const token = extractBearerToken(authorization);
+  let payload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return handleAuthError(res);
+  }
+  req.user = payload;
+  next();
+};
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { getUser } = require("../controllers/users");
+const auth = require("../middlewares/auth");
 
-router.get("/:userId", getUser);
+router.get("/:userId", auth, getUser);
 
 module.exports = router;
