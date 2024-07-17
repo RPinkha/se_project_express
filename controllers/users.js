ff1cc66ca413647547a4d5c82920a2b47215a2c5
RuvenPinkhasov <rubi00010@gmail.com>
2024-07-17 17:24:59 -0400

    fix: fixed the catch for the modifyUser controller

--- controllers/users.js
+++ controllers/users.js
@@ -134,7 +134,24 @@ const modifyUser = (req, res) => {
   )
     .orFail()
     .then((user) => res.send({ name: user.name, avatar: user.avatar }))
-    .catch((err) => console.error(err));
+    .catch((err) => {
+      switch (err.name) {
+        case "CastError":
+          return res.status(BAD_REQUEST).send({
+            message:
+              "invalid data passed to the methods for creating a user, or invalid ID passed to the params.",
+          });
+        case "DocumentNotFoundError":
+          return res.status(NOT_FOUND).send({
+            message:
+              "there is no user with the requested id, or the request was sent to a non-existent address.",
+          });
+        default:
+          return res
+            .status(INTERNAL_SERVER_ERROR)
+            .send({ message: "An error has occurred on the server." });
+      }
+    });
 };
 
 module.exports = { login, createUser, getCurrentUser, modifyUser };
