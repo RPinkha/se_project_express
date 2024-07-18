ebbca47735be0b8f2d5fb8f98eee13aad55ab12b
RuvenPinkhasov <rubi00010@gmail.com>
2024-07-17 20:36:34 -0400

    feat: configured user rights on the delete item controller

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -3,6 +3,7 @@ const {
   CREATED,
   BAD_REQUEST,
   NOT_FOUND,
+  FORBIDDEN,
   INTERNAL_SERVER_ERROR,
 } = require("../utils/errors");
 
@@ -37,9 +38,17 @@ const createItem = (req, res) => {
 
 const deleteItem = (req, res) => {
   const { itemId } = req.params;
+  const loggedInUserId = req.user._id;
+
   Item.findById(itemId)
     .orFail()
-    .then((item) => item.deleteOne())
+    .then((item) => {
+      if (item.owner.toString() === loggedInUserId.toString()) {
+        return item.deleteOne();
+      } else {
+        return Promise.reject({ name: "ForbiddenError" });
+      }
+    })
     .then(() => res.send({ message: "Item deleted successfully" }))
     .catch((err) => {
       switch (err.name) {
@@ -52,6 +61,10 @@ const deleteItem = (req, res) => {
           return res.status(NOT_FOUND).send({
             message:
               "there is no user with the requested id, or the request was sent to a non-existent address.",
+          });
+        case "ForbiddenError":
+          return res.status(FORBIDDEN).send({
+            message: "user does not have permission to delete this item.",
           });
         default:
           return res
