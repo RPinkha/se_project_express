9b1f3da2ef37e1db32c066e5132a017f2d9a6753
RuvenPinkhasov <rubi00010@gmail.com>
2024-07-17 20:38:22 -0400

    feat: installed cors and added it to app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const mainRouter = require("./routes/index");
 
 const { PORT = 3001 } = process.env;
@@ -10,6 +12,7 @@ mongoose
   .then(() => {})
   .catch(console.error);
 
+app.use(cors());
 app.use(express.json());
 app.use("/", mainRouter);
 
