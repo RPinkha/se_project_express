bbeac27b09967b91555f5edb6be9a4adbed9112b
RuvenPinkhasov <rubi00010@gmail.com>
2024-09-21 23:11:10 -0400

    feat: added the logger file and set it up in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
@@ -16,10 +17,12 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+
+app.use(requestLogger);
 app.use("/", mainRouter);
 
+app.use(errorLogger);
 app.use(errors());
-
 app.use(errorHandler);
 
 app.listen(PORT, () => {});
