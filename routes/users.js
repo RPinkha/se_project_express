36690eb0a480859bd2d579966578ba192c6fc855
RuvenPinkhasov <rubi00010@gmail.com>
2024-09-23 23:45:37 -0400

    feat: added validation to users route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const { getCurrentUser, modifyUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
+const { validateUpdateProfile } = require("../middlewares/validation");
 
 router.get("/me", auth, getCurrentUser);
-router.patch("/me", auth, modifyUser);
+router.patch("/me", auth, validateUpdateProfile, modifyUser);
 
 module.exports = router;
